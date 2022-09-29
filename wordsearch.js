dd0e4406f9b825d55c86fdcba2dc61edf1a23cca
Margaryta <margaryta.lanova@gmail.com>
2022-09-28 19:35:20 -0700

    wordsearch pairing program

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,13 @@
 const wordSearch = (letters, word) => { 
+  //if (letters.length === 0 ) return false;
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+        // console.log(l);       
+        if (l.includes(word)) {
+            return true;
+        }
+    }      
+    return false;
+} 
 
 module.exports = wordSearch
